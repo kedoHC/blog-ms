a18ded7f21ac72256374c970198c90f5ed07d012
Jesus Duran <jesusduran@MacBook-Air-de-Jesus.local>
2020-06-22 17:47:57 -0500

    Initial commit

---
+++ event-bus/index.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const axios = require("axios")
+
+const app = express()
+
+app.use(bodyParser.json())
+
+const events = []
+
+app.post("/events", (req, res) => {
+    
+    const event = req.body
+
+    events.push(event)
+    
+    axios.post("http://posts-clusterip-srv:4000/events", event)/*POSTS */
+    axios.post("http://comments-srv:4001/events", event)/*COMMENTS */
+    axios.post("http://query-srv:4002/events", event)/*QUERY-SERVICE */
+    axios.post("http://moderation-srv:4003/events", event)/*MODERATION SERVICES */
+
+    res.send({ status: "OK" })
+
+})
+
+
+app.get("/events", (req, res) => {
+    res.send( events )
+})
+
+app.listen(4005, () => {
+    console.log("EVENT BUS escuchando en puerto: 4005")
+})
